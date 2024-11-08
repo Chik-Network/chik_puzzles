edc785f53a394abfb54d62b52403bc301bc9b14a
Matthew Howard <m.o.howard97@gmail.com>
2024-11-08 16:14:57 +0000

    work for dependency-less hashtree and compile checking

---
+++ chia_puzzles/manage_clvm.py
@@ -0,0 +1,202 @@
+from __future__ import annotations
+
+import dataclasses
+import hashlib
+import json
+import pathlib
+import tempfile
+import traceback
+import typing
+import typing_extensions
+
+from clvm_tools_rs import compile_clvm
+from clvm.SExp import SExp
+from clvm.CLVMObject import CLVMStorage
+
+here = pathlib.Path(__file__).parent.resolve()
+root = here.parent
+cache_path = root.joinpath(".chia_cache", "manage_clvm.json")
+
+# Suffixes and path definitions
+clvm_suffix = ".clvm"
+clsp_suffix = ".clsp"
+hex_suffix = ".clsp.hex"
+all_suffixes = {"clsp": clsp_suffix, "hex": hex_suffix, "clvm": clvm_suffix}
+top_levels = {"chia"}
+hashes_path = root.joinpath("chia-puzzles/deployed_puzzle_hashes.json")
+std_libraries = root.joinpath("chia-puzzles/puzzles")
+
+ValueType = typing.Union[bytes, CLVMStorage]
+ValueStackType = typing.List[ValueType]
+Op = typing.Callable[[ValueStackType, "OpStackType", typing.Set[bytes]], None]
+OpStackType = typing.List[Op]
+
+def std_hash(b: typing.Union[bytes, typing.SupportsBytes], skip_bytes_conversion: bool = False) -> bytes:
+    """
+    The standard hash used in many places.
+    """
+    if skip_bytes_conversion:
+        # casting for hinting based on above overloads constraining the type
+        return hashlib.sha256(typing.cast(bytes, b)).digest()
+    else:
+        return hashlib.sha256(bytes(b)).digest()
+
+def sha256_treehash(sexp: CLVMStorage, precalculated: typing.Optional[typing.Set[bytes]] = None) -> bytes:
+    """
+    Hash values in `precalculated` are presumed to have been hashed already.
+    """
+
+    if precalculated is None:
+        precalculated = set()
+
+    def handle_sexp(sexp_stack: ValueStackType, op_stack: OpStackType, precalculated: typing.Set[bytes]) -> None:
+        # just trusting it is right, otherwise we get an attribute error
+        sexp: SExp = sexp_stack.pop()  # type: ignore[assignment]
+        if sexp.pair:
+            p0, p1 = sexp.pair
+            sexp_stack.append(p0)
+            sexp_stack.append(p1)
+            op_stack.append(handle_pair)
+            op_stack.append(handle_sexp)
+            op_stack.append(roll)
+            op_stack.append(handle_sexp)
+        else:
+            # not a pair, so an atom
+            atom: bytes = sexp.atom  # type: ignore[assignment]
+            if atom in precalculated:
+                r = atom
+            else:
+                r = std_hash(b"\1" + atom)
+            sexp_stack.append(r)
+
+    def handle_pair(sexp_stack: ValueStackType, op_stack: OpStackType, precalculated: typing.Set[bytes]) -> None:
+        # just trusting it is right, otherwise we get a type error
+        p0: bytes = sexp_stack.pop()  # type: ignore[assignment]
+        p1: bytes = sexp_stack.pop()  # type: ignore[assignment]
+        sexp_stack.append(std_hash(b"\2" + p0 + p1))
+
+    def roll(sexp_stack: ValueStackType, op_stack: OpStackType, precalculated: typing.Set[bytes]) -> None:
+        p0 = sexp_stack.pop()
+        p1 = sexp_stack.pop()
+        sexp_stack.append(p0)
+        sexp_stack.append(p1)
+
+    sexp_stack: ValueStackType = [sexp]
+    op_stack: typing.List[Op] = [handle_sexp]
+    while len(op_stack) > 0:
+        op = op_stack.pop()
+        op(sexp_stack, op_stack, precalculated)
+
+    result: bytes = sexp_stack[0]  # type: ignore[assignment]
+    return result
+
+class ManageClvmError(Exception):
+    pass
+
+class CacheEntry(typing.TypedDict):
+    clsp: str
+    hex: str
+    hash: str
+
+class Cache(typing_extensions.TypedDict):
+    entries: CacheEntries
+    version: CacheVersion
+
+CacheEntries = dict[str, CacheEntry]
+CacheVersion = list[int]
+current_cache_version: CacheVersion = [1]
+
+class CacheVersionError(ManageClvmError):
+    pass
+
+def create_empty_cache() -> Cache:
+    return {"entries": {}, "version": current_cache_version}
+
+def load_cache(file: typing.IO[str]) -> Cache:
+    loaded_cache = typing.cast(Cache, json.load(file))
+    loaded_version = loaded_cache.get("version")
+    if loaded_version != current_cache_version:
+        raise CacheVersionError("Cache has wrong version.")
+    return loaded_cache
+
+def dump_cache(cache: Cache, file: typing.IO[str]) -> None:
+    json.dump(cache, file, indent=2)
+
+def generate_hash_bytes(hex_bytes: bytes) -> bytes:
+    cleaned_blob = bytes.fromhex(hex_bytes.decode("utf-8"))
+    serialized_hash = sha256_treehash(cleaned_blob)
+    return serialized_hash
+
+@dataclasses.dataclass(frozen=True)
+class ClvmPaths:
+    clvm: pathlib.Path
+    hex: pathlib.Path
+    hash: str
+    missing_files: list[str]
+
+    @classmethod
+    def from_clvm(cls, clvm: pathlib.Path, hash_dict: dict[str, str] = {}) -> ClvmPaths:
+        stem_filename = clvm.name[: -len(clsp_suffix)]
+        hex_path = clvm.with_name(stem_filename + hex_suffix)
+        missing_files = []
+        if not hex_path.exists():
+            missing_files.append(str(hex_path))
+        return cls(clvm=clvm, hex=hex_path, hash=stem_filename, missing_files=missing_files)
+
+@dataclasses.dataclass(frozen=True)
+class ClvmBytes:
+    hex: bytes
+    hash: bytes
+
+    @classmethod
+    def from_clvm_paths(cls, paths: ClvmPaths, hash_dict: dict[str, str] = {}) -> ClvmBytes:
+        hex_bytes = paths.hex.read_bytes()
+        return cls(
+            hex=hex_bytes,
+            hash=generate_hash_bytes(hex_bytes=hex_bytes)
+        )
+
+def find_stems(top_levels: set[str], suffixes: typing.Mapping[str, str] = all_suffixes) -> dict[str, set[pathlib.Path]]:
+    return {
+        name: {
+            path.with_name(path.name[: -len(suffix)])
+            for top_level in top_levels
+            for path in root.joinpath(top_level).rglob(f"**/*{suffix}")
+        }
+        for name, suffix in suffixes.items()
+    }
+
+def create_cache_entry(reference_paths: ClvmPaths, reference_bytes: ClvmBytes) -> CacheEntry:
+    source_bytes = reference_paths.clvm.read_bytes()
+
+    clvm_hasher = hashlib.sha256()
+    clvm_hasher.update(source_bytes)
+
+    hex_hasher = hashlib.sha256()
+    hex_hasher.update(reference_bytes.hex)
+
+    hash_hasher = hashlib.sha256()
+    hash_hasher.update(reference_bytes.hash)
+
+    return {
+        "clsp": clvm_hasher.hexdigest(),
+        "hex": hex_hasher.hexdigest(),
+        "hash": hash_hasher.hexdigest(),
+    }
+
+def check_cache(cache: Cache, HASHES: dict[str, str]) -> bool:
+    overall_fail = False
+    try:
+        cache_entries = cache["entries"]
+        found_stems = find_stems(top_levels)
+        for stem_path in sorted(found_stems["clsp"]):
+            reference_paths = ClvmPaths.from_clvm(clvm=stem_path.with_name(stem_path.name + clsp_suffix), hash_dict=HASHES)
+            if reference_paths.missing_files:
+                continue
+            reference_bytes = ClvmBytes.from_clvm_paths(paths=reference_paths, hash_dict=HASHES)
+            new_cache_entry = create_cache_entry(reference_paths=reference_paths, reference_bytes=reference_bytes)
+            cache_entries[str(stem_path)] = new_cache_entry
+        return not overall_fail
+    except Exception as e:
+        traceback.print_exc()
+        return False
--- chia_puzzles/tests/test_loads.py
+++ chia_puzzles/tests/test_loads.py
@@ -1,4 +1,194 @@
+import json
+import os
+import pathlib
+import tempfile
+import traceback
+import typing
+from chia_puzzles.load_clvm import compile_clvm
 from chia_puzzles.programs import CAT_PUZZLE
+from chia_puzzles.manage_clvm import Cache, ClvmBytes, ClvmPaths, ManageClvmError, create_cache_entry, create_empty_cache, dump_cache, find_stems, load_cache
+
+here = pathlib.Path(__file__).parent.resolve()
+root = here.parent
+cache_path = root.joinpath(".chia_cache", "manage_clvm.json")
+# Suffixes and path definitions
+clvm_suffix = ".clvm"
+clsp_suffix = ".clsp"
+hex_suffix = ".clsp.hex"
+all_suffixes = {"clsp": clsp_suffix, "hex": hex_suffix, "clvm": clvm_suffix}
+top_levels = {"chia"}
+hashes_path = root.joinpath("chia-puzzles/deployed_puzzle_hashes.json")
+std_libraries = root.joinpath("chia-puzzles/puzzles")
+
+excludes: typing.Set[str] = set()
+
+CacheVersion = typing.List[int]
+
+class CacheVersionError(ManageClvmError):
+    pass
+
+
+class NoCacheVersionError(CacheVersionError):
+    def __init__(self) -> None:
+        super().__init__("Cache must specify a version, none found")
+
+
+class WrongCacheVersionError(CacheVersionError):
+    def __init__(self, found_version: object, expected_version: CacheVersion) -> None:
+        self.found_version = found_version
+        self.expected_version = expected_version
+        super().__init__(f"Cache has wrong version, expected {expected_version!r} got: {found_version!r}")
+
 
 def test_import():
     assert CAT_PUZZLE is not None
+
+def test_check(use_cache: bool) -> int:
+    used_excludes = set()
+    overall_fail = False
+
+    HASHES: typing.Dict[str, str] = json.loads(hashes_path.read_text()) if hashes_path.exists() else {}
+
+    cache: Cache
+    if not use_cache:
+        cache = create_empty_cache()
+    else:
+        try:
+            print(f"Attempting to load cache from: {cache_path}")
+            with cache_path.open(mode="r") as file:
+                cache = load_cache(file=file)
+        except FileNotFoundError:
+            print("Cache not found, starting fresh")
+            cache = create_empty_cache()
+        except NoCacheVersionError:
+            print("Ignoring cache due to lack of version")
+            cache = create_empty_cache()
+        except WrongCacheVersionError as e:
+            print(f"Ignoring cache due to incorrect version, expected {e.expected_version!r} got: {e.found_version!r}")
+            cache = create_empty_cache()
+
+    cache_entries = cache["entries"]
+    cache_modified = False
+
+    found_stems = find_stems(top_levels)
+    found = found_stems["hex"]
+    suffix = all_suffixes["hex"]
+    extra = found - found_stems["clsp"]
+
+    print()
+    print(f"Extra {suffix} files:")
+
+    if len(extra) == 0:
+        print("    -")
+    else:
+        overall_fail = True
+        for stem in extra:
+            print(f"    {stem.with_name(stem.name + suffix)}")
+
+    print()
+    print("Checking that no .clvm files begin with `(mod`")
+    for stem_path in sorted(found_stems["clvm"]):
+        with open(stem_path.with_name(stem_path.name + clvm_suffix)) as file:
+            file_lines = file.readlines()
+            for line in file_lines:
+                non_comment: str = line.split(";")[0]
+                if "(" in non_comment:
+                    paren_index: int = non_comment.find("(")
+                    if len(non_comment) >= paren_index + 4 and non_comment[paren_index : paren_index + 4] == "(mod":
+                        overall_fail = True
+                        print(f"FAIL    : {stem_path.name + clvm_suffix} contains `(mod`")
+                    break
+
+    missing_files: typing.List[str] = []
+    all_hash_stems: typing.List[str] = []
+
+    print()
+    print("Checking that all existing .clsp files compile to .clsp.hex that match existing caches:")
+    for stem_path in sorted(found_stems["clsp"]):
+        clsp_path = stem_path.with_name(stem_path.name + clsp_suffix)
+        if clsp_path.name in excludes:
+            used_excludes.add(clsp_path.name)
+            continue
+
+        file_fail = False
+        error = None
+
+        cache_key = str(stem_path)
+        try:
+            reference_paths = ClvmPaths.from_clvm(clvm=clsp_path, hash_dict=HASHES)
+            if reference_paths.missing_files != []:
+                missing_files.extend(reference_paths.missing_files)
+                continue
+            all_hash_stems.append(reference_paths.hash)
+            reference_bytes = ClvmBytes.from_clvm_paths(paths=reference_paths, hash_dict=HASHES)
+
+            new_cache_entry = create_cache_entry(reference_paths=reference_paths, reference_bytes=reference_bytes)
+            existing_cache_entry = cache_entries.get(cache_key)
+            cache_hit = new_cache_entry == existing_cache_entry
+
+            if not cache_hit:
+                with tempfile.TemporaryDirectory() as temporary_directory:
+                    generated_paths = ClvmPaths.from_clvm(
+                        clvm=pathlib.Path(temporary_directory).joinpath(reference_paths.clvm.name),
+                        hash_dict=HASHES,
+                    )
+
+                    compile_clvm(
+                        input_path=os.fspath(reference_paths.clvm),
+                        output_path=os.fspath(generated_paths.hex),
+                        search_paths=[os.fspath(reference_paths.clvm.parent), str(std_libraries)],
+                    )
+
+                    generated_bytes = ClvmBytes.from_hex_bytes(hex_bytes=generated_paths.hex.read_bytes())
+
+                if generated_bytes != reference_bytes:
+                    file_fail = True
+                    error = f"        reference: {reference_bytes!r}\n"
+                    error += f"        generated: {generated_bytes!r}"
+                else:
+                    cache_modified = True
+                    cache_entries[cache_key] = new_cache_entry
+        except Exception:
+            file_fail = True
+            error = traceback.format_exc()
+
+        if file_fail:
+            print(f"FAIL    : {clsp_path}")
+            if error is not None:
+                print(error)
+        else:
+            print(f"    pass: {clsp_path}")
+
+        if file_fail:
+            overall_fail = True
+
+    if missing_files != []:
+        overall_fail = True
+        print()
+        print("Missing files (run tools/manage_clvm.py build to build them):")
+        for filename in missing_files:
+            print(f" - {filename}")
+
+    unused_excludes = sorted(excludes - used_excludes)
+    if len(unused_excludes) > 0:
+        overall_fail = True
+        print()
+        print("Unused excludes:")
+
+        for exclude in unused_excludes:
+            print(f"    {exclude}")
+
+    extra_hashes = HASHES.keys() - all_hash_stems
+    if len(extra_hashes) != 0:
+        overall_fail = True
+        print()
+        print("Hashes without corresponding files:")
+        for extra_hash in extra_hashes:
+            print(f"    {extra_hash}")
+
+    if use_cache and cache_modified:
+        cache_path.parent.mkdir(parents=True, exist_ok=True)
+        with cache_path.open(mode="w") as file:
+            dump_cache(cache=cache, file=file)
+    
+    assert not overall_fail
