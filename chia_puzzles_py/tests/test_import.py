f5d5f0f043c4dc66dfce3f0b700fdef56a26440f
Matthew Howard <m.o.howard97@gmail.com>
2024-12-12 14:26:52 +0000

    add test of last file in list also

--- chia_puzzles_py/tests/test_import.py
+++ chia_puzzles_py/tests/test_import.py
@@ -1,5 +1,6 @@
-from chia_puzzles_py.programs import CAT_PUZZLE
+from chia_puzzles_py.programs import CAT_PUZZLE, SINGLETON_TOP_LAYER
 
 
 def test_import_CAT_PUZZLE():
     assert CAT_PUZZLE is not None
+    assert SINGLETON_TOP_LAYER is not None
--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,9 @@
 #[cfg(test)]
 mod tests {
-    use crate::loaded_chialisp::CAT_PUZZLE;
+    use crate::loaded_chialisp::{CAT_PUZZLE, SINGLETON_TOP_LAYER};
     #[test]
     fn puzzle_hashes() {
         assert!(CAT_PUZZLE.len() == 1672);
+        assert!(SINGLETON_TOP_LAYER.len() == 1168);
     }
 }
