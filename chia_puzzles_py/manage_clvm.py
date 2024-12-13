3d76619f096de480113a6ebb5a2d846c8325c11a
Matthew Howard <m.o.howard97@gmail.com>
2024-12-13 13:39:15 +0000

    change dependency from clvm to clvm_rs

--- chia_puzzles_py/manage_clvm.py
+++ chia_puzzles_py/manage_clvm.py
@@ -1,10 +1,6 @@
-import io
-
-from clvm.serialize import sexp_from_stream
-from clvm_tools.sha256tree import sha256tree
-from clvm import to_sexp_f
+from clvm_rs import Program
 
 
 def generate_hash_bytes(clvm_bytes: bytes) -> bytes:
-    serialized_hash = sha256tree(sexp_from_stream(io.BytesIO(clvm_bytes), to_sexp_f))
+    serialized_hash = Program.from_bytes(clvm_bytes).tree_hash()
     return serialized_hash
